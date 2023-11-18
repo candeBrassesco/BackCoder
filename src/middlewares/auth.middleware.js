0963a082fc65034136bb2bf1bc497f5afa2488df
Candela Brassesco <candebrassesco@gmail.com>
2023-11-18 13:56:05 -0300

    products doc

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,10 @@
 import productManager from "../dal/dao/mongoManagers/ProductManager.js"
-import { usersModel } from "../dal/db/models/users.models.js"
 
 export const addProdAuth = async ( req, res, next ) => {
     try {
         const {user} = req
         if (user.role === "user") {
-            res.status(400).json({message: 'Not authorized'})
+            res.status(401).json({message: 'Not authorized'})
         }
         next()
     } catch (error) {
@@ -17,7 +16,7 @@ export const chatAuth = async ( req, res, next ) => {
     try {
         const {user} = req
         if(user.role === "admin") {
-            res.status(400).json({message:'Not authorized'})
+            res.status(401).json({message:'Not authorized'})
         }
         next()
     } catch (error) {
@@ -31,7 +30,7 @@ export const addProdCartAuth = async ( req, res, next ) => {
         const {pid} = req.params
         const product = await productManager.getProductById(pid)
         if (user.role === "admin" || user.email === product.owner) {
-           res.status(400).json({message:'Not authorized'})
+           res.status(401).json({message:'Not authorized'})
         }
         next()
     } catch (error) {
@@ -45,10 +44,10 @@ export const delOrUpAuth = async ( req, res, next ) => {
         const {pid} = req.params
         const product = await productManager.getProductById(pid)
         if(user.role === "user") {
-            res.status(400).json({message:'Not authorized'})
+            res.status(401).json({message:'Not authorized'})
         }
         if(user.role ==="premium" && product.owner !== user.email) {
-            res.status(400).json({message: 'Not authorized'})
+            res.status(401).json({message: 'Not authorized'})
         }
         next() 
     } catch(error) {
@@ -60,7 +59,7 @@ export const changeRolAuth = async ( req, res, next ) => {
     try {
         const {user} = req
         if(user.role === "admin") {
-            res.status(400).json({message: 'Not authorized'})
+            res.status(401).json({message: 'Not authorized'})
         }
     } catch (error) {
         return error
